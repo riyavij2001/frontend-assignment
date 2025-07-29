1eb2ca683f912546542bed23d12c7c6957c32dc5
Riya <riya.vij@cyraacs.com>
2025-07-29 10:06:21 +0530

    changed config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   base: process.env.VITE_BASE_PATH || 'frontend'
 })
