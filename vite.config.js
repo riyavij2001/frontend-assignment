fa5692123e8af4e9cefae44ca9a6f3619dd6e813
Riya <riya.vij@cyraacs.com>
2025-07-29 10:22:38 +0530

    Vercel changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
+
   base: process.env.VITE_BASE_PATH || 'frontend'
 })
