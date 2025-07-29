95c7d384317022e109bda8bd39e4762135e7222a
Riya Vij <60844207+riyavij2001@users.noreply.github.com>
2025-07-29 10:37:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: process.env.VITE_BASE_PATH || 'frontend'
+  base: process.env.VITE_BASE_PATH || '/'
 })
